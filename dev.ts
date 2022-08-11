21952c55720a5e3a779b6263e10131ff0b3edeb7
Caleb Cox <canac@users.noreply.github.com>
2022-08-11 15:42:41 -0500

    Add dotenv

--- dev.ts
+++ dev.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S deno run -A --watch=routes/
 
 import dev from "$fresh/dev.ts";
+import { config } from "dotenv";
+
+config({ export: true, safe: true });
 
 await dev(import.meta.url, "./main.ts");
