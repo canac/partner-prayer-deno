eb252a78a4136be616de80ab1116613141272644
Caleb Cox <canac@users.noreply.github.com>
2022-08-11 16:01:35 -0500

    Set document title

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,8 @@
 /** @jsx h */
-import { h } from "preact";
+/** @jsxFrag Fragment */
+import { Fragment, h } from "preact";
 import { tw } from "@twind";
+import { Head } from "$fresh/runtime.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import Partners from "../islands/Partners.tsx";
 import { loadPartners } from "../db.ts";
@@ -18,11 +20,16 @@ export const handler: Handlers<IndexData> = {
 
 export default function Index({ data }: PageProps<IndexData>) {
   return (
-    <main class={tw`bg-gray-800 text-center text-gray-200`}>
-      <h1 class={tw`text-4xl font-bold py-8`}>Partner Prayer</h1>
-      <Partners
-        partners={data.partners}
-      />
-    </main>
+    <>
+      <Head>
+        <title>Partner Prayer</title>
+      </Head>
+      <main class={tw`bg-gray-800 text-center text-gray-200`}>
+        <h1 class={tw`text-4xl font-bold py-8`}>Partner Prayer</h1>
+        <Partners
+          partners={data.partners}
+        />
+      </main>
+    </>
   );
 }
